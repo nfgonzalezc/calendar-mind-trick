882fd22d29bf1b6f3825ec78f58ae00131926c72
nfgonzalezc <nicolas.gonzalez@avaldigitallabs.com>
2019-09-13 11:54:09 -0500

    it was added styles

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from "@stencil/core";
+import { Component, Prop, h, State } from "@stencil/core";
 
 @Component({
   tag: "adl-calendar",
@@ -9,12 +9,44 @@ export class Calendar {
   @Prop() first: string;
   @Prop() middle: string;
   @Prop() last: string;
+  @Prop({ reflect: true }) titleCard: string;
+  @State() showCard: boolean = false;
+
+  year(e) {
+    console.log(e);
+    this.titleCard = "Selecciona el año";
+    console.log(this.titleCard);
+    this.showCard = !this.showCard;
+  }
+  month(e) {
+    console.log(e);
+    this.titleCard = "Selecciona el mes";
+  }
+  day(e) {
+    console.log(e);
+  }
 
   render() {
-    return <div class="calendar">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>;
+    return (
+      <div class='calendar'>
+        <div class='calendar__input'>
+          <input
+            onClick={e => {
+              this.year(e);
+            }}
+            placeholder="Year"
+          />
+          /
+          <input
+            onClick={e => {
+              this.month(e);
+            }}
+            placeholder="Month"
+          />
+          /<input placeholder="Day" />
+        </div>
+        {this.showCard && <div class='calendar__card'>{this.titleCard}</div>}
+      </div>
+    );
   }
 }
